f2b82f96a1e5dda4b47dfb444cec7db1d517759b
jbshipman <jbshipman@icloud.com>
2020-04-26 13:39:12 -0500

    semantic ui react installed

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header as="h1">Home</Header>
+      </div>
+    );
+  }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Header } from "semantic-ui-react";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <Header as="h1">DevCamp React Starter</Header>
+        <Header as="h2">React Redux Router</Header>
       </div>
     );
   }
